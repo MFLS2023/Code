6868fd20e24c36e04cb8fef87f695657519a0099
MFLS2023 <zc23@zua.edu.cn>
2024-09-09 20:51:44 +0800

    004_面板类与继承

---
+++ Java/Chess0909_gaoqilan/src/tech/gaoqilan/main/GamePanel_Gaoqilan.java
@@ -0,0 +1,13 @@
+package tech.gaoqilan.main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel_Gaoqilan extends JPanel {
+
+
+    @Override   //重写注解，如果该方法不是重写父类的方法，加上会报错
+    public void paint(Graphics g) {
+        //super.paint(g);//注释或删除即可
+    }
+}
